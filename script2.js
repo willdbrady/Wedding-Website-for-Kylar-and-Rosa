fa54e86f29c0e16910fb78ee3b5c29a70f8fb263
Will Brady <willdbrady@yahoo.com>
2024-04-11 16:41:03 -0500

    Version 1.0 pre-launch (feedback has already been received by client, she wants a lot of revisions to the work so expect lots of things to change)

--- script2.js
+++ script2.js
@@ -1,11 +1,35 @@
+//hamburger sliding menu
+
+var slide = document.getElementById('slide');
+var burgericon = document.getElementById('burgericon');
+burgericon.addEventListener('click', function (e) {
+  this.classList.toggle('open');
+  slide.classList.toggle('open');
+});
+slide.addEventListener('click', function () {
+  slide.classList.toggle('open');
+  burgericon.classList.toggle('open');
+});
+
+//hamburger sliding menu
+
+var rsvpslide = document.getElementById('rsvpslide');
+var rsvpbutton = document.getElementById('rsvpbutton');
+rsvpbutton.addEventListener('click', function (e) {
+  this.classList.toggle('open');
+  rsvpslide.classList.toggle('open');
+});
+rsvpslide.addEventListener('click', function () {
+  rsvpslide.classList.toggle('open');
+  rsvpbutton.classList.toggle('open');
+});
+
+//Countdown timer for engagement party
 (function () {
   const second = 1000,
     minute = second * 60,
     hour = minute * 60,
     day = hour * 24;
-
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
   let today = new Date(),
     dd = String(today.getDate()).padStart(2, "0"),
     mm = String(today.getMonth() + 1).padStart(2, "0"),
@@ -36,3 +60,16 @@
       }
     }, 0)
 }());
+
+//Code for submitting the form and displaying the finished message.
+
+const submitbutton = document.getElementById("submitbutton");
+const tym = document.getElementById("thankyoumessage");
+const form = document.getElementById("form");
+const label = document.getElementById("label");
+
+form.addEventListener('submit', function () {
+  form.style.display = "none";
+  label.style.display = "none";
+  tym.style.display = "flex";
+});
